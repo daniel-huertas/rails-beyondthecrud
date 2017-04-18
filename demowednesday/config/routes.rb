d9efab7dfec1c9879235a82d010fd90a7c06265d
Daniel Huertas <danielhuertascompany@gmail.com>
2017-04-19 01:00:49 +0200

    new routes for chef and top restaurant & review model

--- demowednesday/config/routes.rb
+++ demowednesday/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants do
+    collection do
+      get "top" => "restaurants#top"
+    end
+    member do
+      get "chef" => "restaurants#chef"
+    end
+  end
 end
